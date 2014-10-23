6b1e6cbd9e8ab88ee8be97ead2e045631a561444
matias <matias@matias-S400CA>
2014-10-23 09:40:55 -0300

    hasta aca esta bien, el anterior commit me eliminó algunas cosas

---
+++ stateless-ejb/src/main/java/entities/Plan.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author matias
+ */
+@Entity
+@Table(name = "plan")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Plan.findAll", query = "SELECT p FROM Plan p"),
+    @NamedQuery(name = "Plan.findByIdPlan", query = "SELECT p FROM Plan p WHERE p.idPlan = :idPlan"),
+    @NamedQuery(name = "Plan.findByNombrePlan", query = "SELECT p FROM Plan p WHERE p.nombrePlan = :nombrePlan"),
+    @NamedQuery(name = "Plan.findByAnioPlan", query = "SELECT p FROM Plan p WHERE p.anioPlan = :anioPlan"),
+    @NamedQuery(name = "Plan.findByCodigoPlan", query = "SELECT p FROM Plan p WHERE p.codigoPlan = :codigoPlan")})
+public class Plan implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID_PLAN")
+    private Integer idPlan;
+    @Size(max = 128)
+    @Column(name = "NOMBRE_PLAN")
+    private String nombrePlan;
+    @Column(name = "ANIO_PLAN")
+    private Integer anioPlan;
+    @Size(max = 32)
+    @Column(name = "CODIGO_PLAN")
+    private String codigoPlan;
+    @JoinColumn(name = "ID_CARRERA", referencedColumnName = "ID_CARRERA")
+    @ManyToOne
+    private Carrera idCarrera;
+    @OneToMany(mappedBy = "idPlan")
+    private List<Asignatura> asignaturaCollection;
+
+    public Plan() {
+    }
+
+    public Plan(Integer idPlan) {
+        this.idPlan = idPlan;
+    }
+    
+    public Integer getIdPlan() {
+        return idPlan;
+    }
+
+    public void setIdPlan(Integer idPlan) {
+        this.idPlan = idPlan;
+    }
+
+    public String getNombrePlan() {
+        return nombrePlan;
+    }
+
+    public void setNombrePlan(String nombrePlan) {
+        this.nombrePlan = nombrePlan;
+    }
+
+    public Integer getAnioPlan() {
+        return anioPlan;
+    }
+
+    public void setAnioPlan(Integer anioPlan) {
+        this.anioPlan = anioPlan;
+    }
+
+    public String getCodigoPlan() {
+        return codigoPlan;
+    }
+
+    public void setCodigoPlan(String codigoPlan) {
+        this.codigoPlan = codigoPlan;
+    }
+
+    public Carrera getIdCarrera() {
+        return idCarrera;
+    }
+
+    public void setIdCarrera(Carrera idCarrera) {
+        this.idCarrera = idCarrera;
+    }
+
+    @XmlTransient
+    public List<Asignatura> getAsignaturaCollection() {
+        return asignaturaCollection;
+    }
+
+    public void setAsignaturaCollection(Collection<Asignatura> asignaturaCollection) {
+        this.asignaturaCollection = (List<Asignatura>) asignaturaCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idPlan != null ? idPlan.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Plan)) {
+            return false;
+        }
+        Plan other = (Plan) object;
+        if ((this.idPlan == null && other.idPlan != null) || (this.idPlan != null && !this.idPlan.equals(other.idPlan))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.Plan[ idPlan=" + idPlan + " ]";
+    }
+    
+}
---
+++ stateless-web/src/main/java/managedbeans/PlanController.java
@@ -0,0 +1,243 @@
+package managedbeans;
+
+import entities.Asignatura;
+import entities.Plan;
+import managedbeans.util.JsfUtil;
+import managedbeans.util.JsfUtil.PersistAction;
+import sessionbeans.PlanFacadeLocal;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.ejb.EJBException;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import javax.faces.model.SelectItem;
+import javax.faces.model.SelectItemGroup;
+import org.apache.commons.lang.StringEscapeUtils;
+
+@Named("planController")
+@SessionScoped
+public class PlanController implements Serializable {
+
+    @EJB
+    private PlanFacadeLocal ejbFacade;
+    private List<Plan> items = null;
+    private Plan selected;
+    private List<SelectItem> listaPlanes;
+    private String selection;
+
+    public PlanController() {
+    }
+
+    public Plan getSelected() {
+        return selected;
+    }
+
+    public void setSelected(Plan selected) {
+        this.selected = selected;
+    }
+
+    protected void setEmbeddableKeys() {
+    }
+
+    protected void initializeEmbeddableKey() {
+    }
+
+    private PlanFacadeLocal getFacade() {
+        return ejbFacade;
+    }
+
+    public Plan prepareCreate() {
+        selected = new Plan();
+        initializeEmbeddableKey();
+        return selected;
+    }
+    
+    public List<SelectItem> getListaPlanes(){
+        this.listaPlanes = new ArrayList<SelectItem>();
+        for (int i = 0; i < items.size(); i++) {
+            SelectItemGroup group = new SelectItemGroup(items.get(i).getNombrePlan());
+            SelectItem[] seleccion = new SelectItem[items.get(i).getAsignaturaCollection().size()];
+            for (int j = 0; j < items.get(i).getAsignaturaCollection().size(); j++) {
+                String a = items.get(i).getAsignaturaCollection().get(j).getNombreAsignatura();
+                SelectItem option = new SelectItem(items.get(i).getAsignaturaCollection().get(j).getNombreAsignatura());
+                seleccion[j] = option;
+            }
+            group.setSelectItems(seleccion);
+            listaPlanes.add(group);
+        }
+        return listaPlanes;
+    }
+    
+    public void create() {
+        persist(PersistAction.CREATE, ResourceBundle.getBundle("/Bundle").getString("PlanCreated"));
+        if (!JsfUtil.isValidationFailed()) {
+            items = null;    // Invalidate list of items to trigger re-query.
+        }
+    }
+
+    public void update() {
+        persist(PersistAction.UPDATE, ResourceBundle.getBundle("/Bundle").getString("PlanUpdated"));
+    }
+
+    public void destroy() {
+        persist(PersistAction.DELETE, ResourceBundle.getBundle("/Bundle").getString("PlanDeleted"));
+        if (!JsfUtil.isValidationFailed()) {
+            selected = null; // Remove selection
+            items = null;    // Invalidate list of items to trigger re-query.
+        }
+    }
+
+    public List<Plan> getItems() {
+        if (items == null) {
+            items = getFacade().findAll();
+        }
+        return items;
+    }
+
+    private void persist(PersistAction persistAction, String successMessage) {
+        if (selected != null) {
+            setEmbeddableKeys();
+            try {
+                if (persistAction != PersistAction.DELETE) {
+                    getFacade().edit(selected);
+                } else {
+                    getFacade().remove(selected);
+                }
+                JsfUtil.addSuccessMessage(successMessage);
+            } catch (EJBException ex) {
+                String msg = "";
+                Throwable cause = ex.getCause();
+                if (cause != null) {
+                    msg = cause.getLocalizedMessage();
+                }
+                if (msg.length() > 0) {
+                    JsfUtil.addErrorMessage(msg);
+                } else {
+                    JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+                JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
+            }
+        }
+    }
+
+    public Plan getPlan(java.lang.Integer id) {
+        return getFacade().find(id);
+    }
+
+    public List<Plan> getItemsAvailableSelectMany() {
+        return getFacade().findAll();
+    }
+
+    public List<Plan> getItemsAvailableSelectOne() {
+        return getFacade().findAll();
+    }
+
+    @FacesConverter(forClass = Plan.class)
+    public static class PlanControllerConverter implements Converter {
+
+        @Override
+        public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
+            if (value == null || value.length() == 0) {
+                return null;
+            }
+            PlanController controller = (PlanController) facesContext.getApplication().getELResolver().
+                    getValue(facesContext.getELContext(), null, "planController");
+            return controller.getPlan(getKey(value));
+        }
+
+        java.lang.Integer getKey(String value) {
+            java.lang.Integer key;
+            key = Integer.valueOf(value);
+            return key;
+        }
+
+        String getStringKey(java.lang.Integer value) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(value);
+            return sb.toString();
+        }
+
+        @Override
+        public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
+            if (object == null) {
+                return null;
+            }
+            if (object instanceof Plan) {
+                Plan o = (Plan) object;
+                return getStringKey(o.getIdPlan());
+            } else {
+                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, "object {0} is of type {1}; expected type: {2}", new Object[]{object, object.getClass().getName(), Plan.class.getName()});
+                return null;
+            }
+        }
+
+    }
+    
+    public String toJSON(Integer planId){
+        StringBuilder jsonB = new StringBuilder();
+        jsonB.append("var context = [");
+        Plan plan = getPlan(planId);
+        
+        ArrayList<Asignatura> as;
+        as = new ArrayList<>(plan.getAsignaturaCollection());
+        for (Asignatura a : as) {
+            ArrayList <Asignatura> pre, post;
+            jsonB.append("{ \"nombre\": \"");
+            jsonB.append(a.getNombreAsignatura());
+            jsonB.append("\", \"id\": ");
+            jsonB.append(a.getCodigoAsignatura());
+            jsonB.append(", \"nivel\": ");
+            jsonB.append(a.getNivelAsignatura());
+            jsonB.append(", \"anual\": ");
+            jsonB.append( a.getEsAnual() == 1 ? "true" : "false" );
+            jsonB.append(", \"sct\": ");
+            jsonB.append(a.getSctAsignatura());
+            jsonB.append(", \"t\": ");
+            jsonB.append(a.getHorasTeoria());
+            jsonB.append(", \"e\": ");
+            jsonB.append(a.getHorasEjercicio());
+            jsonB.append(", \"l\": ");
+            jsonB.append(a.getHorasLaboratorio());
+            jsonB.append(", \"resumen\": \"");
+             jsonB.append(StringEscapeUtils.escapeJavaScript(a.getResumenAsignatura()));
+            //requisitos
+            pre = new ArrayList<> (a.getAsignaturaCollection());
+            jsonB.append("\", \"prerequisitos\": [");
+            for (Asignatura p : pre) {
+                jsonB.append(p.getCodigoAsignatura());
+                if ( pre.lastIndexOf(p) != pre.size()-1 ) jsonB.append(",");
+            }
+            jsonB.append("], \"aperturas\": [");
+            //apertura
+            post = new ArrayList<> (a.getAsignaturaCollection1());
+            for (Asignatura p : post) {
+                jsonB.append(p.getCodigoAsignatura());
+                if ( post.lastIndexOf(p) != post.size()-1 ) jsonB.append(",");
+            }
+            jsonB.append("] }");
+            if (as.lastIndexOf(a) != as.size()-1) jsonB.append(",");
+        }
+        jsonB.append("];");
+        return jsonB.toString() ;
+    }
+    
+     public String getSelection() {
+        return selection;
+    }
+    public void setSelection(String selection) {
+        this.selection = selection;
+    }
+
+}
