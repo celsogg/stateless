98fa36b14359a13aa2dc7648cfa0bcd3a90a7081
matias <matias@matias-S400CA>
2014-11-02 03:00:03 -0300

    usuarios arreglados, falta la persistencia de los datos

--- stateless.sql
+++ stateless.sql
@@ -22,26 +22,27 @@ USE `stateless`;
 -- Table structure for table `usuario`
 --
 
-DROP TABLE IF EXISTS `usuario`;
+DROP TABLE IF EXISTS `USUARIO`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `usuario` (
-  `ID` int(11) NOT NULL,
-  `APELLIDO` varchar(255) DEFAULT NULL,
-  `NOMBRE` varchar(255) DEFAULT NULL,
-  `ROL` varchar(255) DEFAULT NULL,
-  `UID` varchar(255) DEFAULT NULL,
+CREATE TABLE `USUARIO` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `APELLIDO` varchar(128) DEFAULT NULL,
+  `NOMBRE` varchar(128) DEFAULT NULL,
+  `ROL` varchar(20) DEFAULT NULL,
+  `UID` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `usuario`
+-- Dumping data for table `USUARIO`
 --
 
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+LOCK TABLES `USUARIO` WRITE;
+/*!40000 ALTER TABLE `USUARIO` DISABLE KEYS */;
+INSERT INTO `USUARIO` VALUES (1,'Carcamo','Miguel','alumno','1010'),(2,'sor','profe','profesor','1001');
+/*!40000 ALTER TABLE `USUARIO` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
