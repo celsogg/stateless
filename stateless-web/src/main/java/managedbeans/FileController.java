d2705623a10e35baee38fb78bfad241e6ef1f846
Giovanni Benussi <giovanni.benussi@usach.cl>
2014-11-18 08:52:39 -0300

    Borre iconos barra superior

--- stateless-web/src/main/java/managedbeans/FileController.java
+++ stateless-web/src/main/java/managedbeans/FileController.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package managedbeans;
 
 import java.io.BufferedReader;
@@ -11,37 +10,51 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 
 /**
  *
  * @author miguel
  */
-public class FileController {
+@Named("fileController")
+@SessionScoped
+public class FileController extends HttpServlet implements Serializable   {
+
     private File archivo = null;
     private FileReader fr = null;
     private BufferedReader br = null;
-    
-    public void init(ServletConfig config) throws ServletException, FileNotFoundException {
-        ServletContext sc = config.getServletContext();
-        String path = sc.getRealPath("/")+"WEB-INF/logs/logTransacciones.logs";
-        this.archivo = new File (path);
-        this.fr = new FileReader (archivo);
+
+    @Override
+    public void init() {
+//        ServletContext sc = config.getServletContext();
+        ServletContext sc = this.getServletContext();
+        String path = sc.getRealPath("/") + "WEB-INF/logs/logTransacciones.logs";
+        this.archivo = new File(path);
+        try {
+            this.fr = new FileReader(archivo);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(FileController.class.getName()).log(Level.SEVERE, null, ex);
+        }
         this.br = new BufferedReader(fr);
     }
-    
-    public void lectura() throws IOException{
-        try{
+
+    public void lectura() throws IOException {
+        try {
             String linea;
-            while((linea=this.br.readLine())!=null){
+            while ((linea = this.br.readLine()) != null) {
                 System.out.println(linea);
             }
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
-    
+
 }
